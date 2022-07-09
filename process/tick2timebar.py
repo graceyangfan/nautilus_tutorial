5a2c1d392d2e3102fa291d3cf6a3f1f4352818aa
graceyangfan <yf1825617022@outlook.com>
2022-07-09 22:31:32 +0800

    Update tick2timebar.py

--- process/tick2timebar.py
+++ process/tick2timebar.py
@@ -31,7 +31,7 @@ from nautilus_trader.model.identifiers import Symbol
 from nautilus_trader.model.identifiers import Venue
 
 def _ts_parser(time_in_secs: str) -> datetime:
-    return datetime.utcfromtimestamp(int(time_in_secs) / 1_000_000.0)
+    return datetime.utcfromtimestamp(int(time_in_secs) / 1_000.0)
 
 
 def tick2timebar(
@@ -40,50 +40,59 @@ def tick2timebar(
         bar_spec = BarSpecification(10, BarAggregation.SECOND, PriceType.LAST),
     ):
 
-    clock = TestClock()
-    bar_store = ObjectStorer()
-    handler = bar_store.store
-    bar_type = BarType(instrument.id, bar_spec)
-
-    aggregator = TimeBarAggregator(
-            instrument,
-            bar_type,
-            handler,
-            clock,
-            Logger(clock),
-        )
-
     wrangler = TradeTickDataWrangler(instrument=instrument)
     ticks = wrangler.process(pd.read_csv(
             filename,
             index_col="timestamp",
             date_parser=_ts_parser,
             parse_dates=True,
+            nrows=10
         ))
+    clock = TestClock()
+    start_time = ticks[0].ts_event
+    clock.set_time(start_time)
+    handler = []
+    bar_type = BarType(instrument.id, bar_spec)
+
+    aggregator = TimeBarAggregator(
+            instrument,
+            bar_type,
+            handler.append,
+            clock,
+            Logger(clock),
+        )
+    #ticks = ticks[:100]
 
     for tick in ticks:
         aggregator.handle_trade_tick(tick)
-    events = clock.advance_time(ticks[-1].ts_event)
-    for event in events:
-        event.handle()
-    bars = bar_store.get_store()   
-    return bars 
+        events = clock.advance_time(tick.ts_event)
+        if len(events) < 1:
+            continue  
+        for event in events:
+            event.handle()
+    print(handler)
+    return handler 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--filename',default="processedGALBUSD-trades-2022-06.csv")
+    parser.add_argument('--filename',default="processedAPEBUSD-trades-2022-06.csv")
     parser.add_argument('--symbol',default='APEBUSD')
     parser.add_argument("--venue",default='BINANCE') 
     args = parser.parse_args()
-    catalog = ParquetDataCatalog("catalog")
-    from get_all_instruments import write_all_instruments 
+    catalog = ParquetDataCatalog("../catalog/.")
+    #from get_all_instruments import write_all_instruments 
     import json 
     global data 
     with open('config.json') as f:
         data = json.load(f)
-    instrument_id = InstrumentId(Symbol(f"{args.symbol}-PERP"),Venue("BINANCE"))
-    instrument = asyncio.run(write_all_instruments(data["api_key"],data["secret_key"],instrument_id))  
+   # instrument_id = InstrumentId(Symbol(f"{args.symbol}-PERP"),Venue("BINANCE"))
+    instrument_id = f"{args.symbol}-PERP.{args.venue}"
+    instrument = catalog.instruments(instrument_ids=[instrument_id],as_nautilus=True)[0]
+   # instrument = asyncio.run(write_all_instruments(data["api_key"],data["secret_key"],instrument_id))  
     #
+    #from nautilus_trader.serialization.arrow.serializer import register_parquet
+    
     bars = tick2timebar(instrument,args.filename)
     write_objects(catalog, chunk = bars)
+    write_objects(catalog, chunk = [instrument])
