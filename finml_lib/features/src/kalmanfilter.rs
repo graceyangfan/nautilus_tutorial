cad01004f4c2f9582cf4f3adafffe4af788c3bd0
graceyangfan <1825617022.yf@gmail.com>
2023-11-25 13:26:32 +0800

    fixed kalmanfilter variables name

--- finml_lib/features/src/kalmanfilter.rs
+++ finml_lib/features/src/kalmanfilter.rs
@@ -6,6 +6,7 @@ use nalgebra::SMatrix;
 #[derive(Debug)]
 pub struct KalmanFilter {
     state_mean: SMatrix<f64,2,1>, // state_mean for kalman filter,[beta1,beta0]^T (2,1)
+    y: SMatrix::<f64, 1, 1>, 
     q: f64,// moving noise 
     r: f64,//observe noise 
     k: SMatrix<f64,2,1>,//kalman gain 
@@ -20,6 +21,7 @@ impl KalmanFilter {
     pub fn new(q:f64, r:f64, p:f64) -> Self {
         Self {
             state_mean: SMatrix::<f64, 2, 1>::zeros(),
+            y: SMatrix::<f64, 1, 1>::zeros(),
             q:q,
             r:r,
             k: SMatrix::<f64, 2, 1>::zeros(),
@@ -29,6 +31,7 @@ impl KalmanFilter {
     }
     
     pub fn update_raw(&mut self, input_x: f64, input_y: f64) {
+        // y = b*x + a + epsilon_t = [x,1]*[b,a]^T + epsilon_t
         self.initialized = true;
         //forward
         let f = SMatrix::<f64, 2, 2>::identity();
@@ -36,10 +39,11 @@ impl KalmanFilter {
         let p_pred = (f * self.p * f.transpose()).add_scalar(self.q);
         // Correct
         let h = SMatrix::<f64, 1, 2>::from_vec(vec![input_x,1.]);
-        let y = (-h*x_pred).add_scalar(input_y);// Innovation
+        self.y = (-h*x_pred).add_scalar(input_y);// Innovation
+        //println!("{}", self.y);
         let s = (h * p_pred * h.transpose()).add_scalar(self.r); // Innovation covariance
         self.k = p_pred * h.transpose() * s.try_inverse().unwrap(); // Kalman gain 2*2*2*1*1 = (2,1)
-        self.state_mean = x_pred + self.k * y; // Updated state estimate (2,1)+(2,1) = (2,1)
+        self.state_mean = x_pred + self.k * self.y; // Updated state estimate (2,1)+(2,1) = (2,1)
         self.p = (SMatrix::<f64, 2, 2>::identity() - self.k * h) * p_pred; // Updated covariance estimate
     }
 
@@ -51,12 +55,16 @@ impl KalmanFilter {
         self.initialized
     }
 
-    pub fn intercept(&self) -> f64 {
+    pub fn slope(&self) -> f64 {
         self.state_mean[(0,0)]
     }
 
-    pub fn residual(&self) -> f64 {
+    pub fn intercept(&self) -> f64 {
         self.state_mean[(1,0)]
+    }
+
+    pub fn residual(&self) -> f64 {
+        self.y[(0,0)]
     }
 }
 
